66484bd386cfe3deca017f2fa5770b3efd0d2606
Iqra Shaikh <shaikhiqra1503@gmail.com>
2024-06-19 19:42:16 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os 
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%n_%d_%Y_%H_%M_%S')}.log"
+log_path=os.path.join(os.getcwdb(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
+
+
+LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    
+)
+
+if __name__=="__main":
+    logging.info("logging has started")
+    
+    
